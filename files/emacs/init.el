554ac63466181d5a4adcae50dff564cff55605fc
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-01-14 21:15:53 +0100

    feat(i3): keybindings and ricing

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -38,3 +38,16 @@
 
 ;; Keymaps 
 (load-file (concat salorak/home-dir "keymaps/init.el"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(markdown-mode kotlin-mode lua-mode haskell-mode rust-mode go-mode tide php-mode org-bullets org-roam doom-modeline projectile rg counsel ivy corfu marginalia helpful hl-todo magit evil-nerd-commenter evil-surround evil-collection evil undo-fu general doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
