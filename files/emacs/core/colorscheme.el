807fbcb6a8c5639cb40ca1eb5cfaa1109cfddb8c
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-02-06 21:35:46 +0100

    style(modus-vivendi): custom theme

--- files/emacs/core/colorscheme.el
+++ files/emacs/core/colorscheme.el
@@ -10,24 +10,43 @@
 ; Transparent background
 ;; (add-to-list 'default-frame-alist '(alpha-background . 99)) 
 
-(use-package doom-themes
+(use-package modus-themes
   :ensure t
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'modus-vivendi t)
+  ;; Customizations
+  (setq modus-themes-bold-constructs nil)
+  (setq modus-themes-prompts '(bold ))
+  (setq modus-themes-org-blocks 'tinted-background)
+  (setq modus-themes-completions  
+	(quote ((matches . (extrabold underline))
+		(selection . (semibold underline)))))
 
-  ;; Corrects (and improves) org-mode's native fontification. 
-  (doom-themes-org-config)
+  (setq modus-themes-common-palette-overrides
+	'(
+	  ;; Modeline Active
+	  (fg-mode-line-active yellow-faint)
+	  (bg-mode-line-active bg-main)
+	  (border-mode-line-active bg-yellow-intense)
+
+	  ;; Modeline (save)
+	  (modeline-err red-intense)
+
+	  ;; Prompt
+	  (fg-prompt yellow-faint)
+
+	  ;; Region preserve text colors
+	  (bg-region bg-sage)
+	  (fg-region unspecified)
+
+	  ;; Relative numbers -- Gold Like
+	  (fg-line-number-active yellow-faint)
+	  (bg-line-number-active unspecified)
+	  (bg-line-number-inactive unspecified)
+
+	  ;; Comment
+	  (comment yellow-warmer)
+	  ))
+
+  ;; Palette overrides
+  (load-theme 'modus-vivendi-tritanopia t)
   )
-
-;;s(use-package )
-;; (setq themes-list
-;;       '(
-;; 	doom-material-dark
-;; 	doom-tomorrow-night
-;; 	modus-vivendi
-;; 	deeper-blue
-;; 	;; leuven-dark 
-;; 	))
