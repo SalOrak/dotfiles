4eb281f393baa633c84c54f19fb58a7deba21fce
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-06-14 19:11:19 +0200

    feat: shell-pop now works with tmux

--- files/emacs/plugins/shell-pop.el
+++ files/emacs/plugins/shell-pop.el
@@ -2,8 +2,15 @@
 (use-package shell-pop
   :ensure t
   :custom
-  (setq shell-pop-default-directory "~")
-  (setq shell-window-pop-position "bottom")
-  (setq shell-pop-autocd-to-working-dir nil)
+
+  (shell-pop-shell-type '("term" "*term*" (lambda nil (term shell-pop-term-shell))))
+  (shell-pop-term-shell "/bin/bash --login -c 'tmux new-session -A -s emacs'") 
+  (shell-pop-default-directory "~")
+  (shell-pop-window-position "right")
+  (shell-pop-window-size 40)
+  (shell-pop-autocd-to-working-dir nil)
+  (shell-pop-universal-key "M-t")
+  (shell-pop-full-span t)
   )
+
 
