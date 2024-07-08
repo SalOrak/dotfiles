b91dee3a94c9d6a385565c6e7706eb96629669fa
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-07-08 14:10:50 +0200

    feat(fzf): fzf now dynamicall respects .gitignore

--- files/emacs/core/path.el
+++ files/emacs/core/path.el
@@ -2,6 +2,7 @@
 (use-package exec-path-from-shell
   :ensure t
   :config
+  (setq exec-path-from-shell-variables '("PATH" "MANPATH" "FZF_DEFAULT_COMMAND"))
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize))
   (when (daemonp)
