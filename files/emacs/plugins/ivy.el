1280e77f1299ffe1bb201b69ed0458d06c641610
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-07-08 23:17:01 +0200

    chore(ivy): added some goodies for ivy

--- files/emacs/plugins/ivy.el
+++ files/emacs/plugins/ivy.el
@@ -3,10 +3,15 @@
 (use-package ivy
   :config
   (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t
-	enable-remote-dir-locals t
-	ivy-initial-inputs-alist nil ;; Remove initial input in all comands
-	ivy-use-selectable-prompt t ;; Prompt can be selected
+  (setq
+   ivy-use-virtual-buffers nil
+   ivy-display-style 'fancy
+   enable-remote-dir-locals t
+   ivy-magic-slash-non-match-action 'ivy-magic-slash-non-match-create
+   ivy-initial-inputs-alist nil ;; Remove initial input in all comands
+   ivy-use-selectable-prompt t ;; Prompt can be selected
+   ivy-count-format "[%d | %d] "
+   ivy-wrap nil
 	;; (setcdr (assoc 'counsel-M-x ivy-initial-inputs-alist) "") ;; Remove initial input in counsel-M-x command (only)
 	)
   )
