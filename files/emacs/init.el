40f74eec87d697d6a024f70380a6d9b22eddff3b
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-07-15 15:43:41 +0200

    feat(emacs|custom-file): updated custom vars file

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -31,13 +31,13 @@
 ;; Increase amount of data which Emacs read from the process
 (setq read-process-output-max (* 1024 1024))
 
-;; Path example
-;; (setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "/opt/software/openjdk-22/bin")))
-;; (setq exec-path (append exec-path (list (expand-file-name "/opt/software/openjdk-22/bin"))))
-
-
 ;; Home directory
 (defconst salorak/home-dir "~/.config/emacs/")
+
+;; Change the custom-file location
+(setq custom-file (concat salorak/home-dir ".custom.el"))
+(when (f-file-p custom-file)
+  (load custom-file))
 
 ;; Custom
 (load-file (concat salorak/home-dir "custom/init.el"))
@@ -55,3 +55,16 @@
 (when (f-file-p (concat salorak/home-dir "shadow/init.el"))
   (load-file (concat salorak/home-dir "shadow/init.el"))
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-mode restclient ivy-avy shell-pop dumb-jump zoom-window ace-window find-file-in-project elpy typescript-mode yaml-mode markdown-mode kotlin-mode lua-mode haskell-mode rust-mode go-mode tide php-mode org-bullets org-roam doom-modeline ztree rg counsel ivy cape corfu marginalia helpful hl-todo magit evil-nerd-commenter evil-surround evil-collection evil undo-fu hydra general amx nerd-icons f doom-themes gruvbox-theme exec-path-from-shell)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
