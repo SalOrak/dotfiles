a7abb56320619ec6cac6454ab2922f26b15d23ff
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-07-21 20:42:47 +0200

    feat(emacs|corfu): added corfu goodies

--- files/emacs/plugins/corfu.el
+++ files/emacs/plugins/corfu.el
@@ -2,9 +2,13 @@
 ;; Enhances in-buffer completion with a small completion popup.
 ;; It is the minimalistic in-buffer completion counterpart of the Vertico package.
 (use-package corfu
+  :bind
+  ("C-c d" . corfu-info-documentation)
   :custom
   (corfu-cycle t)
   (corfu-auto t)
   (corfu-auto-prefix 2)
   :init
-  (global-corfu-mode))
+  (global-corfu-mode)
+  (corfu-echo-mode)  ;; Displays a brief candidate documentation in the echo area.
+  )
