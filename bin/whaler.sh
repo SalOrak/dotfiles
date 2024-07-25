06d32bb17bda12cbbe99d2e042ff8bdf1089380d
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-07-25 16:11:59 +0200

    feat(whaler.sh): whaler for bash :)

---
+++ bin/whaler.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+
+# In order to works it must be executed in the same shell ( source or (.) )
+goTo() {
+    if [[ -d $1 ]];then
+        echo "Entering into $1..."
+        cd $1
+    fi
+}
+
+# Pass directly a dir to go to
+if [[ -d $1 ]]; then
+    selected=$1
+fi
+
+# Static directories (oneoff whaler dir)
+dirs=( ~/Downloads ~/org)
+
+# Recursive subdirectories
+fzf_dirs=( ~/personal ~/programming )
+fzf_dirs=$(find ${fzf_dirs[@]} -mindepth 1 -maxdepth 1 -type d)
+
+
+# Append static dirs to found dirs
+for d in ${fzf_dirs[@]}
+do
+    dirs[${#dirs[@]}]=$d
+done
+
+# To easily pipe it
+pipe() {
+    for d in ${dirs[@]}
+    do
+        echo $d
+    done
+}
+
+selected=$(pipe | fzf --height=30% --layout=reverse --border=rounded --color=dark)
+
+goTo "$selected"
