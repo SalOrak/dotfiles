59cf7f83fd1e0acf12086399d70e0b13bfab0ea7
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-07-27 18:19:46 +0200

    feat(compile|emacs): emacs compilation script upgraded

--- bin/compile/emacs.sh
+++ bin/compile/emacs.sh
@@ -8,11 +8,16 @@ mkdir -p /opt/software/
 # Go to directory
 cd /opt/software
 
-# Git Clone
-git clone git://git.savannah.gnu.org/emacs -b emacs-29.3 --single-branch --depth=1  /opt/software/emacs
+if [[ ! -d /opt/software/emacs ]]; then
+	# Git Clone
+	git clone git://git.savannah.gnu.org/emacs -b emacs-29.3 --single-branch --depth=1  /opt/software/emacs
+fi
 
 # Go git root dir
 cd /opt/software/emacs
+
+### Dependencies
+sudo apt install -y autoconf
 
 # Create autoconf
 ./autogen.sh
@@ -24,18 +29,24 @@ sudo apt install -y libgnutls28-dev
 sudo apt install -y libjansson-dev
 sudo apt install -y libtree-sitter-dev
 sudo apt install -y libtinfo-dev libncurses-dev
-sudo apt install -y libgccjit-11-dev
+sudo apt install -y libgccjit-12-dev # Version should be the same of gcc
+sudo apt install -y libmagickwand-dev # ImageMagick deps
+sudo apt install -y libxpm-dev
+sudo apt install -y libgif-dev
 
 # Configure Emacs
-./configure \ 
---with-pgtk \
---without-x \
+./configure \
+--with-native-compilation=aot \
+--with-x \
+--with-imagemagick \
 --with-json \
 --with-mailutils \
 --with-tree-sitter \
---with-json 
+--with-json \
+CFLAGS="-O2 -march=native"
 
 # Make 
 make -j$(nproc)
 
-
+# Install
+sudo make install
