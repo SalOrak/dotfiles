96a13b1d9ae2caf8e44805d0d114818457b2c99d
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-08-18 18:01:31 +0200

    feat (emacs|embark): actions on ANYTHING wtf

---
+++ files/emacs/plugins/embark.el
@@ -0,0 +1,19 @@
+;; Embark - Rigth menu click without the mause
+(use-package embark
+  :ensure t
+  :bind
+  (("C-." . embark-act)
+   ("C-;" . emabark-dwim)
+   ("C-h B" . embark-bindings))
+
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :ensure t
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
