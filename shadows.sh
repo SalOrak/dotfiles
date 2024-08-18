00dda500991e045cd4aadfd9ac3e8e74df476a8c
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-08-18 11:43:10 +0200

    feat(shadow): updated script

--- shadows.sh
+++ shadows.sh
@@ -1,9 +1,16 @@
 #!/bin/bash 
 
+
+dots="DOTS"
+
+if [[ $(uname) -eq "Darwin" ]]; then
+    dots="${dots}_MAC"
+fi
+
 # First we execute and install the everything
-./configure.sh INSTALL
+./configure.sh INSTALL --dotfiles $dots
 
 # Then, install the shadow files
-./configure.sh INSTALL --dotfiles ~/personal/shadows/DOTS --start-dir ~/personal/shadows
+./configure.sh INSTALL --dotfiles ~/personal/shadows/$dots --start-dir ~/personal/shadows
 
 
