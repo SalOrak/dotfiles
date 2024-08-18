aaded8c69682db53782a82ff2f5cdbd215329ca0
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-08-18 13:12:56 +0200

    feat(emacs|which-key): updated which-key config

--- files/emacs/plugins/which-key.el
+++ files/emacs/plugins/which-key.el
@@ -1,4 +1,11 @@
 ;; which-key
 (use-package which-key
   :config
+  (setq which-key-dont-use-unicode nil)
+  (setq which-key-separator " → ")
+  (setq which-key-popup-type 'side-window)
+  (setq which-key-side-window-location 'right)
+  (setq which-key-show-docstrings nil)
   (which-key-mode))
+
+
