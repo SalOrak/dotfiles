88c0b0caf78421a5938a5bbd76d71e3538ce395b
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-08-31 15:21:44 +0200

    feat(emacs|cape-compile): improved compile setup

--- files/emacs/plugins/cape.el
+++ files/emacs/plugins/cape.el
@@ -1,7 +1,8 @@
-;; Cape (Completion At Point Extensions)
+;; Cape (Completion At Point Extensions) -*-lexical-binding: t -*-
 ;; The completion backends used by completion-at-point are so called completion-at-point-functions (Capf)
 ;; Registering cape-* functions in the completion-at-point-functions list makes the backends available for
 ;; completion.
+
 (use-package cape
   :after corfu
   :bind ("C-c p" . cape-prefix-map)
@@ -11,3 +12,39 @@
   (add-hook 'completion-at-point-functions #'cape-keyword)
   (add-hook 'completion-at-point-functions #'cape-file)
   )
+
+(defun salorak/compile (prompt)
+  "Prompt for a compile command with Corfu-enabled autocompletion and custom prefix"
+  (interactive)
+  ;; Prompt for the compile command with Corfu enabled in the minibuffer
+  (setq compile-command
+        (minibuffer-with-setup-hook
+            (lambda ()
+                (setq-local completion-at-point-functions
+                            (list
+                             #'cape-file
+                             #'cape-history))
+                (corfu-mode 1))
+          (read-from-minibuffer prompt
+                                (eval compile-command)
+                                nil nil 'compile-history (eval compile-command))))
+  (setq compilation-directory default-directory)
+  ;; Call the compile function interactively
+  (unwind-protect
+      (compilation-start compile-command)))
+
+(defun salorak/compile--rename-buffer (prompt)
+  ""
+  (let
+      ((compilation-buffer (get-buffer "*compilation*"))
+       (new-buffer-name (concat "*" (f-filename default-directory) "-compilation*")))
+    (when compilation-buffer
+      (with-current-buffer compilation-buffer
+        (when (buffer-live-p (get-buffer new-buffer-name))
+           (kill-buffer-ask (get-buffer new-buffer-name)))
+        (rename-buffer new-buffer-name)))))
+
+(advice-add 'salorak/compile :after #'salorak/compile--rename-buffer)
+
+
+ 
