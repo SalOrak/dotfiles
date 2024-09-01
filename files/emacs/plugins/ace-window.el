e3edebf9666d1d0d7c31608c67d462e86547ceee
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-09-01 21:41:30 +0200

    style(emacs|ace-window): changed structure of code

--- files/emacs/plugins/ace-window.el
+++ files/emacs/plugins/ace-window.el
@@ -1,13 +1,11 @@
 (use-package ace-window
   :general
   (:keymaps 'override
-    "M-." 'ace-window
-    )
+            "M-." 'ace-window
+            )
   :ensure t
-  :config
-  (setq aw-keys '(?h ?t ?n))
-  (setq aw-dispatch-always t) ;; Action for 2 windows
-  (setq aw-minibuffer-flag t)
+  
+  :init
   (defun sk/aw-kill-buffer (window)
     "Kill the buffer of the selected window using `ace-window'"
     (aw-delete-window window t))
@@ -15,17 +13,22 @@
     "Switch the buffer of the selected window using `ace-window' ala `consult-buffer'"
     (aw-switch-to-window window)
     (consult-buffer))
+  
+  :config
+  (setq aw-keys '(?h ?t ?n))
+  (setq aw-dispatch-always t) ;; Action for 2 windows
+  (setq aw-minibuffer-flag t)
   (setq aw-dispatch-alist
-	'(
-	  (?m aw-swap-window "[M]ove")
-	  (?c aw-copy-window "[C]opy")
-	  (?b sk/aw-consult-switch-buffer "Switch [B]uffer")
-	  (?u aw-split-window-vert "Split Upwards")
-	  (?s aw-split-window-horz "Split Left")
-	  (?w aw-delete-window "Kill [W]indow")
-	  (?k sk/aw-kill-buffer "[K]ill Buffer")
-	  (?d delete-other-windows "[D]elete Other Windows")
-	  (?? aw-show-dispatch-help))
-	)
+	    '(
+	      (?m aw-swap-window "[M]ove")
+	      (?c aw-copy-window "[C]opy")
+	      (?b sk/aw-consult-switch-buffer "Switch [B]uffer")
+	      (?u aw-split-window-vert "Split Upwards")
+	      (?s aw-split-window-horz "Split Left")
+	      (?w aw-delete-window "Kill [W]indow")
+	      (?k sk/aw-kill-buffer "[K]ill Buffer")
+	      (?d delete-other-windows "[D]elete Other Windows")
+	      (?? aw-show-dispatch-help))
+	    )
   )
 
