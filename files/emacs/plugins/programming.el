5884e2ef01d5a94237256c8ae274baf89dad905b
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-09-04 23:29:29 +0200

    feat(emacs|tide): improved tide setup

--- files/emacs/plugins/programming.el
+++ files/emacs/plugins/programming.el
@@ -1,5 +1,23 @@
 (use-package php-mode)
-(use-package tide)
+(use-package tide
+  :ensure t
+  :general
+  (leader-by-mode :keymaps 'typescript-mode-map
+    "c" 'tide-compile-file
+    "d" 'tide-documentation-at-point
+    "r" 'tide-rename-symbol
+    "j" 'tide-jump-to-definition
+    )
+  :config
+  (defun sk/setup-tide-mode()
+    (interactive)
+    (tide-setup)
+    (tide-hl-identifier-mode +1)
+
+    )
+  (add-hook 'typescript-mode-hook #'sk/setup-tide-mode)
+    )
+
 (use-package go-mode)
 (use-package rust-mode)
 (use-package haskell-mode)
@@ -13,3 +31,7 @@
   :ensure t
   :init
   (elpy-enable))
+
+
+
+
