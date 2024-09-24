6f9c7aeca6f165143246a796a3b23f447cf8f4a4
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-09-24 19:22:40 +0200

    feat(emacs|move-text): move lines using arrows for crkbd

--- files/emacs/plugins/move-text.el
+++ files/emacs/plugins/move-text.el
@@ -3,6 +3,8 @@
   :general
   (:keymaps 'override
             "M-p" 'move-text-up
+            "M-<up>" 'move-text-up
+            "M-<down>" 'move-text-down
             "M-n" 'move-text-down
             )
   )
