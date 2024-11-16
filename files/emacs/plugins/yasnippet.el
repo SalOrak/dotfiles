eb018b572655a34df876e0179beb41ac74d21c6f
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-11-16 22:03:30 +0100

    fix(emacs|yasnippet): no more new lines after snippet

--- files/emacs/plugins/yasnippet.el
+++ files/emacs/plugins/yasnippet.el
@@ -10,11 +10,13 @@
     "s p" 'yas-prev-field
     "s R" 'yas-reload-all
     )
+
   (:keymaps '(yas-minor-mode-map)
             "TAB" yas-maybe-expand
             "<tab>" yas-maybe-expand
             )
   :config
+  (setq require-final-newline nil) ; Don't add a newline at the end of the file automatically! Messes with yasnippets 
   (setq salorak/snippets-dir (concat salorak/home-dir "snippets/"))
   (setq yas-snippet-dirs '(salorak/snippets-dir))
   (setq yas-triggers-in-field nil)
