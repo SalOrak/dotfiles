bdc6e1ae76114c3aeea46e2c5bea790677f435e2
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-11-30 16:39:36 +0100

    feat(emacs|hl): don't highlight in vterm

--- files/emacs/plugins/hl-todo.el
+++ files/emacs/plugins/hl-todo.el
@@ -6,4 +6,11 @@
              (global-hl-todo-mode t)
              ;; Highligh current line
              (global-hl-line-mode t)
+
+             (defun sk/hl--disable-line-mode ()
+               "Disable highlight line mode for the current buffer"
+               (set (make-local-variable 'global-hl-line-mode) nil)
+               )
+             (add-hook 'vterm-mode-hook #'sk/hl--disable-line-mode)
+             (add-hook 'vterm-copy-mode-hook #'sk/hl--disable-line-mode)
              )
