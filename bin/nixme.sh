b72274f1e165fa6af3caecc3c40e8b10d3c08e34
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-12-15 12:25:10 +0100

    feat(bash|nixme): modify config file nixos

---
+++ bin/nixme.sh
@@ -0,0 +1,16 @@
+# NixOS configuration script
+# Edits the configuration file, formats it and commits
+
+set -e
+pushd ~/dotfiles/nixos
+NIX_FILE="configuration.nix"
+$EDITOR $NIX_FILE
+emacsclient --create-frame --alternate-editor="" $NIX_FILE
+alejandra $NIX_FILE &>/dev/null
+git diff -U0 *.nix
+echo -e "\n[\e[93mIN PROGRESS\e[0m] Rebuilding NixOS ..."
+sudo nixos-rebuild switch &> /tmp/nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+current=$(nixos-rebuild list-generations | grep current)
+git commit -am "build(nixos): $current"
+echo -e "\n[\e[92mDONE\e[0m] Succesfully rebuilded NixOS!"
+popd
