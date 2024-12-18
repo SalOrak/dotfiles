79701d465771e46babe41e072b341d740b9ba282
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-12-18 11:34:18 +0100

    build(nixos): 16 current  2024-12-18 11:32:46  24.11.710905.a0f3e10d9435  6.6.63                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -171,6 +171,10 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
+  services.syncthing = {
+    enable = true;
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
