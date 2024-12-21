5bd3cdc6bcc1b4b18293172bec4c92e03eb8cf71
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-12-21 21:18:12 +0100

    build(nixos): 5 current   2024-12-21 21:18:09  24.11.710905.a0f3e10d9435  6.6.63                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -172,6 +172,17 @@
   #   enableSSHSupport = true;
   # };
 
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true; # if not already enabled
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
   services.blueman.enable = true; # bluetooth gui
   # List services that you want to enable:
 
