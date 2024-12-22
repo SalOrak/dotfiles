f7074d6eaf4a4c84bae1e391bea4371ceefec169
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-12-22 18:22:33 +0100

    build(nixos): 13 current  2024-12-22 18:22:31  24.11.710905.a0f3e10d9435  6.6.63                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,6 +11,10 @@
     /etc/nixos/hardware-configuration.nix
   ];
 
+  # Enable flakes
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
