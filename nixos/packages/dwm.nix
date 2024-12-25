89f89e8a12e9fa55ee9f4ff4f877af4f33358b21
SalOrak <99770300+SalOrak@users.noreply.github.com>
2024-12-25 20:18:32 +0100

    feat(nix|dwm): custom dwm build :)

--- nixos/packages/dwm.nix
+++ nixos/packages/dwm.nix
@@ -2,14 +2,14 @@
   pkgs ? import <nixpkgs> {}
 }:
 
-
 pkgs.stdenv.mkDerivation rec {
   pname = "dwm";
   version = "6.5";
-  src = pkgs.fetchgit {
-    url = "git://git.suckless.org/dwm";
-    rev = "6.5";
-    hash = "sha256-Cc4B8evvuRxOjbeOhg3oAs3Nxi/msxWg950/eiq536w=";
+  src = pkgs.fetchFromGitHub {
+    owner = "salorak";
+    repo = "dwm-setup";
+    rev = "db5903c61e82ab47088cc36581a6a449a0fd8df1";
+    hash = "sha256-T5zq4NAWx0QdHXxSoAO5eKKoMOEOM5aWzeq19PihprE=";
   };
 
   buildInputs = with pkgs; [
