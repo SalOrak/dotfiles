4a2153e234ee2bcdfbbfe48242361d4db10bf184
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-01-04 12:49:36 +0100

    nixos(audio|bluetooth): improved bluetooth audio

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -203,6 +203,18 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
+    wireplumber = {
+      extraConfig = {
+        bluetoothEnhancements = {
+          "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+            "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+          };
+        };
+      };
+    };
   };
 
   services.blueman.enable = true; # bluetooth gui
