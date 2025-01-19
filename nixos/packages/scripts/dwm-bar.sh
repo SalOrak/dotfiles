af741fe46a88d40e1b972609c3ed02d2df3a518e
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-01-19 17:15:19 +0100

    feat(dwm-bar|network): added network to status bar

--- nixos/packages/scripts/dwm-bar.sh
+++ nixos/packages/scripts/dwm-bar.sh
@@ -3,36 +3,51 @@ sec=0
 
 # Update functions
 update_cpu() {
-  cpu=$(awk '{print $1}' /proc/loadavg)
+    cpu=$(awk '{print $1}' /proc/loadavg)
 }
 
 update_memory() {
-  memory=$(free -h | awk 'NR==2 {print $3}')
+    memory=$(free -h | awk 'NR==2 {print $3}')
 }
 
 update_time() {
-  time=$(date "+%b %d %H:%M ")
+    time=$(date "+%b %d %H:%M ")
+}
+
+update_network() {
+    nint="wlp0s20f3"
+    ipv4=$(ip -o -4 a | grep $nint | awk '{print $4}')
+    ipv6=$(ip -o -6 a | grep $nint | awk '{print $4}')
+    net=""
+    if [[ -n $ipv4 ]]; then
+        net="  $ipv4"
+    elif [[ -n $ipv6 ]]; then
+        net="  $ipv6"
+    else
+        net="󰲜  Disconnected"
+    fi
 }
 
 update_bat() {
-  if [[ -e /sys/class/power_supply/BAT1/status ]]; then
-    bat_status=$(< /sys/class/power_supply/BAT1/status)
-    if [[ -e /sys/class/power_supply/BAT1/capacity ]]; then
-      bat_capacity=$(< /sys/class/power_supply/BAT1/capacity)
-      bat="$bat_status $bat_capacity%"
-    else
-      bat="$bat_status (capacity not available)"
+    if [[ -e /sys/class/power_supply/BAT1/status ]]; then
+        bat_status=$(< /sys/class/power_supply/BAT1/status)
+        if [[ -e /sys/class/power_supply/BAT1/capacity ]]; then
+            bat_capacity=$(< /sys/class/power_supply/BAT1/capacity)
+            bat="$bat_status $bat_capacity%"
+        else
+            bat="$bat_status (capacity not available)"
+        fi
     fi
-  fi
 }
 
 
 # Display function
 display() {
     status=""
-      [[ -n "$bat" ]] && status+="$bat "
-      [[ -n "$time" ]] && status+="$time"
-      xsetroot -name "$status"
+    [[ -n "$net" ]] && status+="$net"
+    [[ -n "$bat" ]] && status+="$bat "
+    [[ -n "$time" ]] && status+="$time"
+    xsetroot -name "$status"
 }
 
 # Set up signal handlers
@@ -43,15 +58,15 @@ update_bat
 
 # Main loop
 while true; do
-  sleep 1
-  sec=$((sec + 1))
-  case $((sec % 60)) in
-    0) update_bat;;
-    [1-5])
-        update_cpu
-        update_time
-        display
-      ;;
-  esac
+    sleep 1
+    sec=$((sec + 1))
+    case $((sec % 60)) in
+        0) update_bat;;
+        [1-5])
+            update_cpu
+            update_time
+            display
+            ;;
+    esac
 done
 
