414d7ece9ccb7047c35a6551c7e11360300c647b
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-03-20 11:26:24 +0100

    feat(emacs|keybinds): better keybinds

--- files/emacs/plugins/emacs.el
+++ files/emacs/plugins/emacs.el
@@ -6,14 +6,14 @@
   ;; ----------------
   (leader-global
     "e" 'compile
+    "." '(async-shell-command :wk "Here Async Shell")
     "s" 'recompile
+    "C-s" 'recompile
     "o" 'sk/compilation-goto-last-running-compilation-buffer
     "3" 'sk/compilation-delete-windows
     "1" 'query-replace-regexp
-    "3" 'sk/compilation-delete-windows
-    "d" 'dired-other-window
+    ;; "d" 'dired-other-window
     ;; "ww" 'save-buffer ;; Save buffers
-    "K" 'general-describe-keybindings
     "C-y" 'sk/absolute-buffer-name-on-clipboard
     )
   ;; ----------------
@@ -26,6 +26,7 @@
     '(emacs-lisp-mode-map lisp-interaction-mode-map)
     "f" 'eval-defun
     "s" 'eval-last-sexp
+    "p" 'pp-eval-last-sexp
     "b" 'eval-buffer
     )
   ;; Compilation mode keybindings
@@ -33,22 +34,31 @@
    '(compilation-mode-map)
    "n" 'compilation-next-error
    "p" 'compilation-previous-error
+   "r" 'recompile
    )
-  (:keymaps 'override
-            "<escape>" 'keyboard-quit
-            "M-(" 'backward-sexp
-            "M-)" 'forward-sexp
-            "C-M-n" 'next-error
-            "C-M-p" 'previous-error
-            )
+  ;; (:keymaps 'override
+  ;;           "<escape>" 'keyboard-quit
+  ;;           ;; "M-(" 'backward-sexp
+  ;;           ;; "M-)" 'forward-sexp
+  ;;           "C-M-n" 'next-error
+  ;;           "C-M-p" 'previous-error
+  ;;           )
   ;; Bookmarks
-  (:keymaps 'override
-            "C-x r D" 'bookmark-delete
-            "C-x r I" 'bookmark-insert
-            "C-x r R" 'bookmark-rename
-            "C-x r C" 'bookmark-relocate
-            "C-x r S" 'bookmark-save
-            )
+  (leader-global :keymaps 'override
+    ;; "C-x r D" 'bookmark-delete
+    ;; "C-x r I" 'bookmark-insert
+    ;; "C-x r R" 'bookmark-rename
+    ;; "C-x r C" 'bookmark-relocate
+    ;; "C-x r S" 'bookmark-save
+    "bd" 'bookmark-delete
+    "bi" 'bookmark-insert
+    "br" 'bookmark-rename
+    "bl" 'bookmark-relocate
+    ;; "bs" 'bookmark-save
+    "bs" 'bookmark-set
+    "bj" 'consult-bookmark
+    )
+  
   ;; Move through the minibuffer history using C-n C-p
   (:keymaps '(minibuffer-mode-map)
    "C-n" 'next-line-or-history-element
@@ -69,6 +79,6 @@
             "C-x C-1" 'sk/window-switch-and-kill-current :wk "Window: Kill current "
             "C-x C-3" 'split-window-right :wk "Split Window Right"
             )
-  :hook
-  (add-hook 'compilation-filter-hook 'sk/colorize-compilation-buffer)
   )
+
+
