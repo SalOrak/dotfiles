291417a10b378179cafa6823390d7c1a9060238a
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-04-16 12:13:03 +0200

    feat(emacs|modeline): added keycast to modeline

--- files/emacs/plugins/modeline.el
+++ files/emacs/plugins/modeline.el
@@ -1,8 +1,15 @@
 (use-package mood-line
   :init
   (defun sk/--mood-whaler-cwd ()
-    (format "[ %s ] " 
-            (capitalize (car (last (f-split (whaler-current-working-directory)))))))
+    (propertize (format "%s" 
+                        (capitalize (car (last (f-split (whaler-current-working-directory))))))
+                'face 'mode-line-emphasis
+                )
+    )
+  (defun sk/--mood-keycast ()
+    (when (mode-line-window-selected-p)
+      sk/keycast--str
+      ))
   
   :custom
   ;; (mood-line-format mood-line-format-default)
@@ -11,10 +18,11 @@
                      :left
                      (" " (mood-line-segment-modal) " "
                       (or (mood-line-segment-buffer-status) " ") " "
-                      (sk/--mood-whaler-cwd) 
+                      "[ " (sk/--mood-whaler-cwd) " ] "
                       (mood-line-segment-buffer-name) "  " (mood-line-segment-anzu) "  "
                       (mood-line-segment-multiple-cursors) "  "
-                      (mood-line-segment-cursor-position) " " (mood-line-segment-scroll)
+                      (mood-line-segment-cursor-position) " " (mood-line-segment-scroll) "  "
+                      (sk/--mood-keycast) " "
                       "")
                      :right
                      ((mood-line-segment-vc) "  " (mood-line-segment-major-mode) "  "
