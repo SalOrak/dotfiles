21dee80072a0252c7449dae1d2b277017e8a9bdb
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-05-13 11:00:06 +0200

    feat(emacs|PDF): Read pdfs in EMACS!!!!!!

--- files/emacs/plugins/latex.el
+++ files/emacs/plugins/latex.el
@@ -21,5 +21,22 @@
 
   )
 
+(use-package pdf-tools
+  :config
+  ;; Installation
+  (pdf-tools-install)
+
+  ;; Customization
+  (setq pdf-view-display-size 'fit-height)
+  
+  
+  (defun sk/pdf-view-pre-setup ()
+    ;; Does not work well with line-numbers-mode. So let's deactivate them
+    (display-line-numbers-mode -1)
+    )
+
+  (advice-add #'pdf-view-mode :after #'sk/pdf-view-pre-setup)
+  )
+
 
 
