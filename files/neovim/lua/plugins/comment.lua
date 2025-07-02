dfb493afb67fd139509b15d718b4f0a036ca0f89
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-07-02 14:45:04 +0200

    feat(nvim|commment): from config() to opts

--- files/neovim/lua/plugins/comment.lua
+++ files/neovim/lua/plugins/comment.lua
@@ -1,10 +1,9 @@
 return {
 	'numToStr/Comment.nvim',
-	config = function()
-		require('Comment').setup({
-			extra = {
-				eol = 'gce' -- End of line 
-			},
-		});
-	end,
+    opts = {
+        extra = {
+            eol = 'gce' -- End of line 
+
+        },
+    }
 }
