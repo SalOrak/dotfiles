971b0e27f55cf9fc572c0c1d6b6138987bc25b63
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-08-22 13:28:29 +0200

    chore(nvim|ansible-doc): a working configuration :)

--- files/neovim/lua/plugins/ansible-doc.lua
+++ files/neovim/lua/plugins/ansible-doc.lua
@@ -1,8 +1,15 @@
 return {
     dir = "/home/hector/personal/ansible-doc.nvim",
-    config = function()
-        local k = vim.keymap.set
-        local ansible = require'ansible-doc'
-        k("n", "<leader>u", ansible.ansible_docs)
-    end
+    dev = true,
+    opts = {
+        cache_pages = false,
+        docs_structure = {
+            "author",
+            "attributes",
+            "notes",
+            "options",
+            "examples",
+            "return",
+        },
+    }
 }
