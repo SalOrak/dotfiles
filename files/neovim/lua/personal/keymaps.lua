802aa497378d7c8a02745e0b935187f9917854b5
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-08-31 11:42:15 +0200

    feat(neovim|keymaps): added new and better documentation

--- files/neovim/lua/personal/keymaps.lua
+++ files/neovim/lua/personal/keymaps.lua
@@ -11,14 +11,14 @@ cmd = function(cmd)
 end
 
 -- Keep centered while page scrolling 
-k.set({"n"}, "<C-d>", "<C-d>zz")
-k.set({"n"}, "<C-u>", "<C-u>zz")
+k.set({"n", "v"}, "<C-d>", "<C-d>zz")
+k.set({"n", "v"}, "<C-u>", "<C-u>zz")
 
 -- Replace q: for : 
 k.set({"n"}, ":", "q:", {remap = true})
 
 -- Replace Esc to C-c
-k.set({"n", "i"}, "<C-c>", "<Esc>", { noremap = true, silent = true})
+k.set({"n", "i", "v"}, "<C-c>", "<Esc>", { noremap = true, silent = true})
 vim.api.nvim_create_autocmd("CmdwinEnter", {
   group = vim.api.nvim_create_augroup("CmdwinMaps", { clear = true }),
   callback = function()
@@ -26,6 +26,18 @@ vim.api.nvim_create_autocmd("CmdwinEnter", {
       k.set({"n"}, "<C-c><C-c>", "<Esc>q<CR>", { noremap = true, silent = true})
   end,
 })
+
+k.set({"n", "v"}, "<leader>:", ":", {desc = "Normal Command Line"})
+
+
+
+-- Copy parent directory path
+k.set({"n"}, "<leader>yy", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, {desc = "Copy parent dir path"})
+-- Copy Whaler directory project path
+k.set({"n"}, "<leader>yw", function() 
+    local path, _ = require('telescope').extensions.whaler.get_current_project()
+    vim.fn.setreg("+", path) 
+end, {desc = "Copy Whaler project path"})
 
 -- Error using quickfix
 k.set({"n"}, "<C-n", cmd("cnext") .. "zz")
@@ -60,22 +72,30 @@ k.set({"n"}, "<leader><leader>P", cmd("FzfLua profiles"))
 k.set({"n"}, "<leader><leader>Q", cmd("FzfLua builtin"))
 
 -- Scratchpad functionality
-k.set({"n"}, "<leader>o", cmd("e ~/.scratchpad.lua"))
-k.set({"n"}, "<leader>O", cmd("so"))
+k.set({"n"}, "<leader>o", cmd("e ~/.scratchpad.lua"), {desc = "Go to Lua Scratchpad"})
+k.set({"n"}, "<leader>O", cmd("so"), {desc = "[so]urce current file"})
 
 -- Vimux 
 k.set({"n"}, "<leader>s", function()
     vim.api.nvim_cmd(vim.api.nvim_parse_cmd("VimuxClearTerminalScreen", {}), {})
     vim.api.nvim_cmd(vim.api.nvim_parse_cmd("VimuxRunLastCommand", {}), {})
-end)
+end,
+{ desc = "[Vimux] Run last command"}
+
+)
 
 
-k.set({"n"}, "<leader>3", cmd("VimuxTogglePane"))
-k.set({"n"}, "<leader>Y", cmd("VimuxZoomRunner"))
-k.set({"n"}, "<leader>e", cmd("VimuxInspectRunner"))
+k.set({"n"}, "<leader>3", cmd("VimuxTogglePane"), {desc = "[Vimux] Toggle pane"})
+k.set({"n"}, "<leader>Y", cmd("VimuxZoomRunner"),{desc = "[Vimux] Zoom"})
+k.set({"n"}, "<leader>e", cmd("VimuxInspectRunner"), {desc = "[Vimux] Inspect"})
 
 -- Telescope Extensions: Whaler
-k.set({"n"}, "<leader>p", require('telescope').extensions.whaler.whaler)
+k.set(
+    {"n"}, 
+    "<leader>p", 
+    require('telescope').extensions.whaler.whaler, 
+    { desc = "[Whaler]"}
+)
 
 
 -- Execute command on current Whaler project or PWD
@@ -92,14 +112,16 @@ k.set({"n"}, "<leader>q", function()
         vim.api.nvim_cmd(vim.api.nvim_parse_cmd("call VimuxRunCommand(\"" .. user_command .. "\")" , {}), {})
         vim.g.VimuxOpenExtraArgs = ""
     end)
-end)
+end,
+{ desc = "[Vimux] Execute command on current Whaler project or directory"}
+)
 
 -- Execute command on current file or PWD
 k.set({"n"}, "<leader>w", function()
     local cwd, _ = require('telescope').extensions.whaler.get_current_project()
     local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
-	vim.g.VimuxOpenExtraArgs = "-c " .. cwd
-	vim.ui.input({ prompt = "" .. filename.. " -- Compile >> ", default = "", completion = "shellcmd" }, function(user_command)
+    vim.g.VimuxOpenExtraArgs = "-c " .. cwd
+    vim.ui.input({ prompt = "" .. filename.. " -- Compile >> ", default = "", completion = "shellcmd" }, function(user_command)
         if not user_command then
             vim.g.VimuxOpenExtraArgs = ""
             return
@@ -107,14 +129,17 @@ k.set({"n"}, "<leader>w", function()
         vim.api.nvim_cmd(vim.api.nvim_parse_cmd("VimuxRunInCurrentFile " .. user_command , {}), {})
         vim.g.VimuxOpenExtraArgs = ""
     end)
-end)
+end,
+{ desc = "[Vimux] Execute command on current file or directory"}
+)
 
 -- Execute whaler without changing cwd
 k.set({"n"}, "<leader>P", function()
 	require('telescope').extensions.whaler.whaler ({
 		auto_cwd = false
 	})
-	end
+	end,
+    { desc = "[Whaler] Preserves CWD"}
 )
 
 -- Find file using whaler without changing current_working directory
@@ -129,7 +154,8 @@ k.set({"n"}, "<leader>F", function()
 			prefix_dir = "",
 		},
 	})
-	end
+	end,
+    { desc = "[Whaler] Find file in another Whaler project"}
 )
 
 -- Grep using whaler without changing current_working directory
@@ -144,13 +170,24 @@ k.set({"n"}, "<leader>R", function()
 			prefix_dir = "",
 		},
 	})
-	end
+	end, 
+    { desc = "[Whaler] Live Grep in another Whaler project"}
 )
 
-
 -- Oil 
-k.set({"n"}, "<leader>d", cmd("Oil")) 
-k.set({"n"}, "<localleader>d", cmd("Oil")) 
+k.set({"n"}, "<leader>d", cmd("Oil"), {desc = "Oil Parent directory"}) 
+k.set({"n"}, "<localleader>d", 
+    function()
+        local oil = require('oil')
+        local cwd = { 
+            path = vim.fn.expand("%:h"),
+            display = vim.fn.expand("%:h")
+        }
+        require('telescope').extensions.whaler.whaler_select_project(cwd)
+        oil.open(cwd.path,nil,nil)
+        require('oil.actions').cd.callback("win",true)
+    end, { desc = "Sets current directory as whaler directory"})
+
 
 -- Custom:
 -- Tmux navigation
@@ -160,22 +197,8 @@ vim.keymap.set({"n"}, "<M-h>", function() tmux_select_pane("Left") end)
 vim.keymap.set({"n"}, "<M-l>", function() tmux_select_pane("Right") end)
 
 
--------- Daily keymaps
--- vim.keymap.set({"n"}, "<leader>m", cmd("e ~/work/ansible/DAILY.md"))
--- vim.keymap.set({"n"}, "<leader><leader>s", function()
---     local time = os.date("%H%M%S-%d-%m-%Y")
---     local filename = "IMG-" .. time 
---     local dir = "/home/hector/work/ansible/media/"
---     local file = dir .. filename
---     local cmd = vim.api.nvim_parse_cmd("!grim -g \"$(slurp)\" -o " .. dir .." ".. file, {})
---     cmd.mods.silent = false
---     cmd.magic.file= false
---     vim.api.nvim_cmd(cmd, { output = false})
--- end)
-
-
 -- Neogit
-vim.keymap.set({"n"}, "<leader>gg", cmd("Neogit"))
+vim.keymap.set({"n"}, "<leader>gg", cmd("Neogit"), {desc = "NeoGit"})
 
 -- Plugin Lua development
 vim.keymap.set({"n"}, "<leader><leader>o", ":AnsibleDoc<CR>")
