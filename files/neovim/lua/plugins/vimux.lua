eb876e01839985c77cce8787377a6a76c11d0376
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-08-31 11:41:21 +0200

    style(vimux): proper indentation

--- files/neovim/lua/plugins/vimux.lua
+++ files/neovim/lua/plugins/vimux.lua
@@ -8,16 +8,16 @@ return {
 		global.VimuxPromptString = "Compile >> "
 		global.VimuxRunnerType = "pane"
 		global.VimuxRunnerName= "[Compile]"
-		vim.api.nvim_create_user_command("VimuxRunInCurrentFile",
-		function(opts)
-			local bufnr = vim.api.nvim_get_current_buf()
-			local bufname = vim.api.nvim_buf_get_name(bufnr):gsub("^oil://", "")
-			local cmd = table.concat(opts.fargs, " ") .. " \'" .. bufname .."\'"
-			vim.api.nvim_cmd(
-				vim.api.nvim_parse_cmd("call VimuxRunCommand(\"" .. cmd.. "\")", {}), 
-				{})
-		end, {nargs = 1})
-		
+        vim.api.nvim_create_user_command(
+            "VimuxRunInCurrentFile",
+            function(opts)
+                local bufnr = vim.api.nvim_get_current_buf()
+                local bufname = vim.api.nvim_buf_get_name(bufnr):gsub("^oil://", "")
+                local cmd = table.concat(opts.fargs, " ") .. " \'" .. bufname .."\'"
+                vim.api.nvim_cmd(
+                    vim.api.nvim_parse_cmd("call VimuxRunCommand(\"" .. cmd.. "\")", {}), 
+                    {})
+                end, {nargs = 1})
 	end,
 
 }
