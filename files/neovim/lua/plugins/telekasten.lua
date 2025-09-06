bed2b979aebca1611061ecc04df533bce565d6b5
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-09-06 19:15:42 +0200

    feat(nvim|telekasten): added telekasten for notes

---
+++ files/neovim/lua/plugins/telekasten.lua
@@ -0,0 +1,30 @@
+return {
+    "nvim-telekasten/telekasten.nvim",
+    dependencies = {"nvim-telescope/telescope.nvim"},
+    config = function()
+        require'telekasten'.setup {
+            home = vim.fn.expand("~/personal/notes"),
+            dailies = vim.fn.expand("~/personal/notes/dailies"),
+            templates = vim.fn.expand("~/personal/notes/templates"),
+            img_subdir = "img",
+            new_note_filename = "title-uuid",
+            uuid_type = "rand",
+            image_link_style = "wiki",
+            vaults = {
+                personal = {
+                    home = vim.fn.expand("~/personal/notes"),
+                    dailies = vim.fn.expand("~/personal/notes/dailies"),
+                    templates = vim.fn.expand("~/personal/notes/templates"),
+                },
+                work = {
+                    home = vim.fn.expand("~/work/notes"),
+                    dailies = vim.fn.expand("~/work/notes/dailies"),
+                    templates = vim.fn.expand("~/work/notes/templates"),
+                }
+            },
+            clipboard_program = "wl-paste",
+            filename_space_subst = "-",
+            filename_small_case = true,
+        }
+    end
+}
