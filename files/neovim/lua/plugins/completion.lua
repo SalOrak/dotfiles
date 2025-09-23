5d535a229bdb9b98fcef6ef6fab611f3f5b72d84
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-09-23 16:34:39 +0200

    feat(neovim|completion): init

---
+++ files/neovim/lua/plugins/completion.lua
@@ -0,0 +1,56 @@
+return {
+	"hrsh7th/nvim-cmp",
+	lazy = false,
+	priority = 100,
+	dependencies = {
+		"onsails/lspkind.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-cmdline",
+	},
+
+	config = function()
+		vim.opt.completeopt = { "menu", "menuone", "noselect", "preview" }
+		vim.opt.shortmess:append("c")
+
+		local cmp = require("cmp")
+
+		cmp.setup({
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "path" },
+				{ name = "buffer" },
+				{ name = "cmdline" },
+			},
+			mapping = {
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-y>"] = cmp.mapping(
+					cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					}),
+					{ "i", "c" }
+				),
+			},
+
+			sorting = {
+				priority_weight = 2,
+				comparators = {
+					-- Below is the default comparitor list and order for nvim-cmp
+					cmp.config.compare.offset,
+					-- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+					cmp.config.compare.exact,
+					cmp.config.compare.score,
+					cmp.config.compare.recently_used,
+					cmp.config.compare.locality,
+					cmp.config.compare.kind,
+					cmp.config.compare.sort_text,
+					cmp.config.compare.length,
+					cmp.config.compare.order,
+				},
+			},
+		})
+	end,
+}
