732989a64520ab2b51e61de9d02345eda1129823
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-11-27 14:14:06 +0100

    feat(nvim|autofmt): autoformat zig

--- files/neovim/lua/plugins/autoformat.lua
+++ files/neovim/lua/plugins/autoformat.lua
@@ -7,14 +7,28 @@ return {
 				lua = { "stylua" },
 				nix = { "alejandra" },
 				java = { "google-java-format" },
+                zig = {"zigfmt"},
+			},
+			default_format_opts = {
+				-- LSP formatting is used when available
+				-- and then other formatters
+				lsp_format = "first",
 			},
 		})
 
 		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = { "*.nix", "*.java", "*.lua" },
+			pattern = { "*.nix", "*.java", "*.lua", "*.zig" },
 			group = vim.api.nvim_create_augroup("sk_autoformatter", { clear = true }),
 			callback = function(args)
-				require("conform").format({ bufnr = args.buf })
+				require("conform").format({ bufnr = args.buf, async = true }, function(err, did_edit) end)
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("InsertLeave", {
+			pattern = { "*.nix", "*.zig"},
+			group = vim.api.nvim_create_augroup("sk_autoformatter", { clear = true }),
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf, async = true }, function(err, did_edit) end)
 			end,
 		})
 	end,
