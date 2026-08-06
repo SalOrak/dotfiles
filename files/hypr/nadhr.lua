77c1e82348342d5acc69a69ceb6f073c3e40e10d
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-08-06 16:22:47 +0200

    hx<>yazi<>hypr: deep integration

--- files/hypr/nadhr.lua
+++ files/hypr/nadhr.lua
@@ -21,7 +21,18 @@ hl.monitor({
 ------------------
 
 local menu        = "wofi"
-local terminal    = apps.alacritty
+
+local terminal    = {
+  exec = "kitty --class=_helix --single-instance --listen-on=unix:/tmp/kitty-helix",
+  class = "_helix",
+  workspace = 1
+}
+local yazi_quick = {
+  exec = "kitty --start-as=hidden kitten quick-access-terminal yazi",
+  class = "yazi-quick",
+  workspace = nil
+
+}
 local fileManager = apps.yazi
 local browser = apps.librewolf
 local notes = apps.obsidian
@@ -71,7 +82,7 @@ hl.bind(withMod("+ T"), libs.debugWindow)
 -- Binds
 hl.bind(withMod("+ Q"), libs.execOrFocus(terminal))
 hl.bind(withMod("+ D"), libs.execOrFocus(browser))
-hl.bind(withMod("+ E"), libs.execOrFocus(fileManager))
+hl.bind(withMod("+ E"), libs.execOrFocus(yazi_quick))
 hl.bind(withMod("+ A"), libs.execOrFocus(notes))
 hl.bind(withMod("+ SPACE"), hl.dsp.exec_cmd(menu))
 
