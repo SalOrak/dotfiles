15d74b4821376faa946f16d7de13570475c81a8f
SalOrak <99770300+SalOrak@users.noreply.github.com>
2026-08-15 12:01:18 +0200

    hypr: added mirror monitor support

--- files/hypr/nadhr.lua
+++ files/hypr/nadhr.lua
@@ -16,6 +16,14 @@ hl.monitor({
     scale    = "1.2",
 })
 
+hl.monitor({
+  output = "",
+  mode = "1920x1080@60",
+  position = "0x0",
+  scale = "1.2",
+  mirror = "eDP-1",
+})
+
 ------------------
 --- VARIABLES ----
 ------------------
